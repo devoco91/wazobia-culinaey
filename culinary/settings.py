e3f64715f8d15f3bbfab4410e5b5521928a2c7c8
David <ashuaib1991@gmail.com>
2025-02-03 20:45:29 +0100

    again

--- culinary/settings.py
+++ culinary/settings.py
@@ -179,13 +179,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# MEDIA_URL=''
+MEDIA_URL=''
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'school/static')
 ]
-# STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
-# MEDIA_ROOT= BASE_DIR/''
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT= BASE_DIR/''
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
